8d5ef29cd45d3f0c98fbc4870700d3a3c85276d0
00Julian00 <87448079+00Julian00@users.noreply.github.com>
2024-04-27 20:04:14 +0200

    Delete 0.1

--- Nova/Modules/GetDateTime.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-def DateTime():
-    return(datetime.datetime.now().strftime("%H:%M %d-%m-%Y"))
-
-def Initialize():
-    pass
--- Nova/StartInterface.py
+++
@@ -1,8 +0,0 @@
-#Opens 'Interface.py' as a new CMD window
-
-import subprocess
-import os
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\Interfacing")
-
-subprocess.Popen(["cmd", "/c", "start", "cmd", "/k", f"title Interface && python Interface.py"])
